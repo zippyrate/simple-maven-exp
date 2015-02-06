b68174799fc4fac4a36567ce70a93a706c608fb2
zippyrate <martin@zippyrate.com>
2015-02-06 15:05:40 -0800

    Added missing exec plugin

--- src/main/java/exp/maven/fat/FatMavenExp.java
+++ src/main/java/exp/maven/fat/FatMavenExp.java
@@ -1,20 +1,19 @@
-
 package exp.maven.fat;
 
 public class FatMavenExp
 {
-		public static void main(String [] argc)
-		{
-				System.out.println("Howdy!");
-		}
+	public static void main(String[] argc)
+	{
+		System.out.println("Howdy!");
+	}
 
-		public static double square(double x)
-		{
-				return x * x;
-		}
+	public static double square(double x)
+	{
+		return x * x;
+	}
 
-		public int add(int x, int y)
-		{
-				return x + y;
-		}
+	public int add(int x, int y)
+	{
+		return x + y;
+	}
 }
